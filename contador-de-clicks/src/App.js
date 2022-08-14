4c54c2e91e6a39b41583a353c110ab41278cf522
Alejandra Aedo <aedo.alejandra@gmail.com>
2022-08-13 23:37:57 -0400

    termino del proyecto

--- contador-de-clicks/src/App.js
+++ contador-de-clicks/src/App.js
@@ -1,14 +1,46 @@
 import './App.css';
-import freeCodeCampLogo from './imagenes/freecodecamplogo.png'
+import freeCodeCampLogoI from './imagenes/freecodecamplogo.jpg';
+import Boton from './componentes/Boton';
+import Contador from './componentes/contador';
+import {useState} from 'react';
 
 function App() {
+  const [numClics, setNumClics]= useState(0);
+
+  const manejarClicks = () => {
+      setNumClics(numClics+1);
+
+
+  };
+  const reiniciarContador = () => {
+    setNumClics(0);
+
+  };
   return (
-    <div className="App">
-     <div className='frerecodecamp-logo-contenedor'>
-      <img 
-      className='freecodecamp-logo'
-      scr={freeCodeCampLogo}
-      alt='Logo de freeCodeCamp'/>
+    <div className='App'>
+     <div className='freecodecamp-logo-contenedor'>
+        <img  
+          className='freecodecamo-logo'   
+          src= {freeCodeCampLogoI}
+          alt='logo'
+       />
+     </div>
+     <div className='contenedor-principal'>
+      <Contador
+      numClics={numClics} />
+        
+      
+      <Boton
+      texto= 'Clic'
+      esBotonDeClick={true} 
+      manejarClic={manejarClicks}   />
+      <Boton
+      texto= 'Reiniciar'
+      esBotonDeClick={false} 
+      manejarClic={reiniciarContador}   />
+
+     
+
      </div>
     </div>
   );
